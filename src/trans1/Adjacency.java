da47e0ccdca9e2ad318cfd3d3d30ab74284826c2
Basket <Basket@10.0.0.3>
2020-01-15 21:37:32 -0700

    Creation

--- src/trans1/Adjacency.java
+++ src/trans1/Adjacency.java
@@ -6,6 +6,7 @@
 package trans1;
 
 import Jama.Matrix;
+import java.util.ArrayList;
 
 /**
  *
@@ -57,7 +58,7 @@ public class Adjacency {
         adj.set(id1 - 1, id2 - 1, 1);
     }
     
-    public void disconnect(int id1, int id2) {
+    public void disconnect(int id1, int id2) { 
         /*
         Disconnects 2 items. Inputs are id's; since deletion not implemented, index = id-1
         After deletion, lookup table or something similar will need to be used.
@@ -74,6 +75,45 @@ public class Adjacency {
         
         adj.print(3, 0);
         
+    }
+    
+    public void connectedness() {
+        //initialize
+        ArrayList<Integer> to_check = new ArrayList<>();
+        ArrayList<Integer> pending = new ArrayList<>();
+        ArrayList<Integer> connected = new ArrayList<>();
+        ArrayList<Integer> temp_list = new ArrayList<>();
+        ArrayList<Integer> list_of_lists = new ArrayList<>();
+        int currid;
+        
+        for(int i=0; i<elements; i++) { //populate initial list of all idxs
+            to_check.add(i);
+        }
+        
+        while(!to_check.isEmpty()) {
+            currid = to_check.get(0);
+            to_check.remove(0);
+            connected.add(currid);
+            
+            for(int i=0; i<elements; i++) { //check for adjacents
+                if(adj.get(i, elements ) == 1) {
+                    //adjacent, add to temp_list
+                    temp_list.add(i);                    
+                }
+            }
+            if(temp_list.size() > 0) {
+                //if you found some, rmv ones that aren't in tocheck
+                for(int i=0; i<temp_list.size(); i++ ) {
+                    if(to_check.contains(temp_list(i))) {
+                        
+                    }
+                }
+                
+                //then add rest to pending & connected
+            }
+        
+        }
+            
     }
 
 }
